40691801dfa77676c4b6c2cc7e84891f28d04e57
meretamal <meretamal@uc.cl>
2020-08-14 13:45:51 -0400

    Uploud ayudantia ruby

---
+++ ayudantias/01_ruby/01_io.rb
@@ -0,0 +1,5 @@
+# Get user input
+data = gets.chomp  # gets = input (leaves the "\n" character), chomp = strip
+
+# Print user input previously recieved
+puts data
---
+++ ayudantias/01_ruby/02_string_interpolation.rb
@@ -0,0 +1,10 @@
+# Define arbitrary element amount
+amount = 6
+
+# Define element name
+element = "meses"
+
+# Compose message
+composition = "Llevo #{amount} largos #{element} encerrado en mi casa"
+
+puts composition
